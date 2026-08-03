1a7b10c744a93e4cb0e7615c0fa404b73a0f22e4
danimelenge <danielmelenge@gmail.com>
2026-08-03 14:18:46 -0500

    Agrega conversión a Alfabeto Fonético NATO como tercera pestaña
    - Nuevo NatoCode: conversión Texto ↔ NATO (Alfa, Bravo, Charlie...),
      incluye letras, números y separador de palabras
    - Nuevo NatoViewModel: pipeline reactivo con Combine, igual patrón
      que MorseViewModel y BrailleViewModel
    - Nueva NatoView: pestaña completa con el mismo lenguaje visual
      que Morse y Braille
    - RootView: agrega natoViewModel, tercera pestaña "NATO" en el
      TabView, y soporte del ícono NATO en la hoja de novedades

---
+++ Signalist/Models/ViewModels/NatoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NatoViewModel.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 3/08/26.
+//
+
+import Foundation
---
+++ Signalist/Views/NatoView.swift
@@ -0,0 +1,8 @@
+//
+//  NatoView.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 3/08/26.
+//
+
+import Foundation
