105de1552a1d4ee6d5acb21bbe91b47babb0bfb6
danimelenge <danielmelenge@gmail.com>
2026-08-19 11:18:15 -0500

    feat: add binary converter

---
+++ Signalist/Models/BinaryCode.swift
@@ -0,0 +1,8 @@
+//
+//  BinaryCode.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 19/08/26.
+//
+
+import Foundation
---
+++ Signalist/Models/ViewModels/BinaryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BinaryViewModel.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 19/08/26.
+//
+
+import Foundation
