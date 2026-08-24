8101ad03448e946a662e17fa2a56ba13d978bf93
danimelenge <danielmelenge@gmail.com>
2026-08-24 13:59:45 -0500

    Agrega conversión a ASCII como quinta pestaña
    - Nuevo AsciiCode: conversión Texto ↔ ASCII (códigos decimales),
      usa UTF-8 para soportar acentos y caracteres especiales
    - Nuevo AsciiViewModel: pipeline reactivo con Combine, mismo patrón
      que Morse, Braille, NATO y Binario
    - Nueva AsciiView: pestaña completa con el mismo lenguaje visual
      que el resto de conversores
    - RootView: agrega asciiViewModel, quinta pestaña "ASCII" en el
      TabView, y soporte del ícono ASCII en la hoja de novedades

---
+++ Signalist/Models/AsciiCode.swift
@@ -0,0 +1,8 @@
+//
+//  AsciiCode.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 24/08/26.
+//
+
+import Foundation
---
+++ Signalist/Views/AsciiView.swift
@@ -0,0 +1,8 @@
+//
+//  AsciiView.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 24/08/26.
+//
+
+import Foundation
