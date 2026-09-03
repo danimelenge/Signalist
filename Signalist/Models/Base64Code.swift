b5e5303552c03c086a8ac828d5a3d3ff9697b707
danimelenge <danielmelenge@gmail.com>
2026-09-03 12:50:40 -0500

    add Base64 converter

---
+++ Signalist/Models/Base64Code.swift
@@ -0,0 +1,8 @@
+//
+//  Base64Code.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 3/09/26.
+//
+
+import Foundation
---
+++ Signalist/Views/Base64View.swift
@@ -0,0 +1,8 @@
+//
+//  Base64View.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 3/09/26.
+//
+
+import Foundation
