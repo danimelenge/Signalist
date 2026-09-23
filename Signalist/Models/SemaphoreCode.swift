052afc7f602b5f307f6fcb5623321f0831d58f83
danimelenge <danielmelenge@gmail.com>
2026-09-23 17:03:25 -0500

    Agrega banderas de Semáforo naval como undécima pestaña
    - Nuevo SemaphoreCode: conversión Texto ↔ Semáforo naval, representa
      cada letra como par de flechas (brazo derecho + izquierdo) según
      la tabla estándar documentada por CEMC (University of Waterloo)
    - Nuevo SemaphoreViewModel: pipeline reactivo con Combine, mismo
      patrón que el resto de conversores de la app
    - Nueva SemaphoreView: pestaña con fuente más grande en el resultado
      para mayor legibilidad de las flechas de dirección
    - RootView: agrega semaphoreViewModel y undécima pestaña "Semáforo"
      en el TabView

---
+++ Signalist/Models/SemaphoreCode.swift
@@ -0,0 +1,8 @@
+//
+//  SemaphoreCode.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 23/09/26.
+//
+
+import Foundation
---
+++ Signalist/Models/ViewModels/SemaphoreViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SemaphoreViewModel.swift
+//  Signalist
+//
+//  Created by Daniel Melenge Rojas on 23/09/26.
+//
+
+import Foundation
